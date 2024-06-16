d03818c63d2d0afa764b150b5da955e07036425a
Mohammadzeineddin <webme2024@gmail.com>
2024-06-16 03:04:56 +0300

    final33

---
+++ script.js
@@ -0,0 +1,17 @@
+/* JavaScript (in script.js file) */
+document.addEventListener("DOMContentLoaded", function() {
+    const videosBtn = document.getElementById("videos-btn");
+    const pdfsBtn = document.getElementById("pdfs-btn");
+    const videosContainer = document.getElementById("videos-container");
+    const pdfsContainer = document.getElementById("pdfs-container");
+  
+    videosBtn.addEventListener("click", function() {
+      videosContainer.style.display = "block";
+      pdfsContainer.style.display = "none";
+    });
+  
+    pdfsBtn.addEventListener("click", function() {
+      videosContainer.style.display = "none";
+      pdfsContainer.style.display = "block";
+    });
+  });
